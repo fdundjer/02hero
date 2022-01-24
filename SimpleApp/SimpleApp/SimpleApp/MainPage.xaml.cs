037760fc46d8d1c81fbfb81883b1245a1f2b1953
Filip Dunđer <fdundjer@outlook.com>
2022-01-24 22:04:41 +0100

    Basic stacklayout implementation

---
+++ SimpleApp/SimpleApp/SimpleApp/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SimpleApp
+{
+    public partial class MainPage : ContentPage
+    {
+        private const string ValidUsername = "02hero";
+        private const string ValidPassword = "predavanje3";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            LoginLabel.IsVisible = true;
+
+            if (PasswordEntry.Text == ValidPassword && UsernameEntry.Text == ValidUsername)
+            {
+                LoginLabel.Text = "Uspešan login";
+            }
+            else
+            {
+                LoginLabel.Text = "Neuspešan login";
+            }    
+        }
+    }
+}
