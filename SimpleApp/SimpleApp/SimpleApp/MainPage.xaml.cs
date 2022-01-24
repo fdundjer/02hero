0e87627f6e0bfb0d9e0f354297f2c3c6e779ec99
Filip Dunđer <fdundjer@outlook.com>
2022-01-24 22:06:20 +0100

    Empty app

--- SimpleApp/SimpleApp/SimpleApp/MainPage.xaml.cs
+++ SimpleApp/SimpleApp/SimpleApp/MainPage.xaml.cs
@@ -10,26 +10,9 @@ namespace SimpleApp
 {
     public partial class MainPage : ContentPage
     {
-        private const string ValidUsername = "02hero";
-        private const string ValidPassword = "predavanje3";
-
         public MainPage()
         {
             InitializeComponent();
-        }
-
-        private void Button_Clicked(object sender, EventArgs e)
-        {
-            LoginLabel.IsVisible = true;
-
-            if (PasswordEntry.Text == ValidPassword && UsernameEntry.Text == ValidUsername)
-            {
-                LoginLabel.Text = "Uspešan login";
-            }
-            else
-            {
-                LoginLabel.Text = "Neuspešan login";
-            }    
         }
     }
 }
