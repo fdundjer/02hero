d0bc98eff8e08c74bb5daf8687cc7b97ca3c4f69
Filip <filip@union.studio>
2022-03-13 21:36:46 +0100

    Added loading/saving to json file

--- SimpleApp/SimpleApp/SimpleApp/DataAccess/NotesRepository.cs
+++ SimpleApp/SimpleApp/SimpleApp/DataAccess/NotesRepository.cs
@@ -1,36 +1,55 @@
 ﻿using SimpleApp.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
+using Newtonsoft.Json;
+using Xamarin.Essentials;
 
 namespace SimpleApp.DataAccess
 {
-    internal class NotesRepository : INotesRepository
-    {
+    public class NotesRepository : INotesRepository
+    {       
         private List<Note> _notes = new List<Note>();
+        private const string FileName = "notes.txt"; 
 
         public NotesRepository()
         {
-            _notes.Add(new Note("This is short title",
-                "This is short description"));
-            _notes.Add(new Note("This is loooooooooooooooooooooooooooooooooooooooooooooooooooooooooong title",
-                "This is lo lo lo lo lo lo lo lo lo lo lo lo lo lo lo lo ng description"));
+            LoadNotes();
         }
 
         public void AddNote(Note note)
         {
             _notes.Add(note);
+            Save();
         }
 
         public void DeleteNote(Guid id)
         {
-            _notes = _notes.Where(note => note.Id != id).ToList();
+            _notes = _notes.Where(n => n.Id != id).ToList();
+            Save();
         }
 
         public IEnumerable<Note> GetAllNotes()
         {
-            return new List<Note>(_notes);
+            return _notes.ToList();
+        }
+
+        private void LoadNotes()
+        {
+            var path = Path.Combine(FileSystem.AppDataDirectory, FileName);
+            if (!File.Exists(path))
+            {
+                return;
+            }
+            
+            var data = File.ReadAllText(path);
+            _notes = JsonConvert.DeserializeObject<List<Note>>(data);
+        }
+        
+        private void Save()
+        {
+            File.WriteAllText(Path.Combine(FileSystem.AppDataDirectory, FileName), JsonConvert.SerializeObject(_notes));
         }
     }
 }
--- SimpleApp/SimpleApp/SimpleApp/Models/Note.cs
+++ SimpleApp/SimpleApp/SimpleApp/Models/Note.cs
@@ -1,32 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Newtonsoft.Json;
 
 namespace SimpleApp.Models
 {
-    internal class Note
+    public class Note
     {
+        [JsonConstructor]
+        public Note(Guid id, string title, string description)
+        {
+            Id = id;
+            if (string.IsNullOrEmpty(title))
+            {
+                throw new InvalidOperationException("Title can't be empty");
+            }
+            
+            if (string.IsNullOrEmpty(description))
+            {
+                throw new InvalidOperationException(nameof(description));
+            }
+
+            Title = title;
+            Description = description;
+        }
+        
         public Note(string title, string description)
         {
             if (string.IsNullOrEmpty(title))
             {
-                throw new InvalidOperationException(nameof(title));
+                throw new InvalidOperationException("Title can't be empty");
             }
 
             if (string.IsNullOrEmpty(description))
             {
                 throw new InvalidOperationException(nameof(description));
             }
-
+            
             Id = Guid.NewGuid();
             Title = title;
             Description = description;
         }
-
+        
         public Guid Id { get; }
-
+        
         public string Title { get; }
-
+        
         public string Description { get; }
     }
 }
